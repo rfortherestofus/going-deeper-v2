d6480e63403def41cb034ebb66d4781084d5abef
David Keyes <david@rfortherestofus.com>
2023-10-11 11:14:21 -0700

    Updates

--- code/pipe-data-into-ggplot-my-turn.R
+++ code/pipe-data-into-ggplot-my-turn.R
@@ -16,7 +16,25 @@ download.file("https://github.com/rfortherestofus/going-deeper-v2/raw/main/data/
 # Import Data -------------------------------------------------------------
 
 third_grade_math_proficiency <- 
-  read_rds("data/third_grade_math_proficiency.rds")
+  read_rds("data/third_grade_math_proficiency.rds") |> 
+  select(academic_year, school, school_id, district, proficiency_level, number_of_students) |> 
+  mutate(is_proficient = case_when(
+    proficiency_level >= 3 ~ TRUE,
+    .default = FALSE
+  )) |> 
+  group_by(academic_year, school, district, school_id, is_proficient) |> 
+  summarize(number_of_students = sum(number_of_students, na.rm = TRUE)) |> 
+  ungroup() |> 
+  group_by(academic_year, school, district, school_id) |> 
+  mutate(percent_proficient = number_of_students / sum(number_of_students, na.rm = TRUE)) |> 
+  ungroup() |> 
+  filter(is_proficient == TRUE) |> 
+  select(academic_year, school, district, percent_proficient) |> 
+  rename(year = academic_year) |> 
+  mutate(percent_proficient = case_when(
+    is.nan(percent_proficient) ~ NA,
+    .default = percent_proficient
+  ))
 
 # Plot --------------------------------------------------------------------
 
--- code/pipe-data-into-ggplot-your-turn.R
+++ code/pipe-data-into-ggplot-your-turn.R
@@ -9,14 +9,21 @@ dir_create("data")
 
 # Download Data -----------------------------------------------------------
 
-download.file("https://rfor.us/enrollment-data",
+download.file("https://github.com/rfortherestofus/going-deeper-v2/raw/main/data/enrollment_by_race_ethnicity.rds",
               mode = "wb",
               destfile = "data/enrollment_by_race_ethnicity.rds")
 
 # Import Data -------------------------------------------------------------
 
 enrollment_by_race_ethnicity <- 
-  read_rds("data/enrollment_by_race_ethnicity.rds")
+  read_rds("data/enrollment_by_race_ethnicity.rds") |> 
+  read_rds("data/enrollment_by_race_ethnicity.rds") |> 
+  select(-district_institution_id)  |> 
+  select(year, district, everything()) |> 
+  mutate(year = case_when(
+    year == "School 2021-22" ~ "2021-2022",
+    year == "School 2022-23" ~ "2022-2023",
+  ))
 
 # Plot --------------------------------------------------------------------
 
